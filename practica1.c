4e17823109d66e3a0bdef97e747bc7b8235eb62b
Leonardo Torres <leonardotorresuni@gmail.com>
2026-05-07 11:10:27 -0500

    practica?final

--- practica1.c
+++ practica1.c
@@ -8,18 +8,40 @@ Descripcion: Rotación de arreglo
 #include <stdio.h>
 #include "vld.h"
 
-int main(int argc, char const *argv[])
+float desplazamiento(int count, int tamano, float vector[],float vec[]);
+
+int main()
 {
     int n = valid_int("Ingrese el tamaño del arreglo: ", 1, 100);
     int k = valid_int("Ingrese el número de posiciones a desplazar: ", 0, n-1);
     float arreglo[n];
-    for (int i = 0; i < n-1; i++)
+    float arreglof[n];
+    for (int i = 0; i < n; i++)
     {
-        arreglo[i] = float_int("Ingrese los numeros que iran dentro del arreglo (-100 a 100)", -100, 100);
+        arreglo[i] = valid_float("Ingrese los numeros que iran dentro del arreglo (-100 a 100):", -100, 100);
+    }
+
+    desplazamiento(k, n, arreglo, arreglof);
+    
+    printf("El vector con los numero desplazados a la derecha es: \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("--%.1f", arreglof[i]);
+    }
+
+    printf("Vector original: \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("--%.1f", arreglo[i]);
     }
     
-
     return 0;
 }
 
-
+float desplazamiento(int count, int tamano ,float vector[], float vec[])
+{
+    for (int i = 0; i < tamano; i++)
+    {
+        vec[(i+count)%tamano] = vector[i];
+    } 
+}
